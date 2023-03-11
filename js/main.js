9d87e960b5aba443ceb24760bcfb07c2d912c19d
ViktorITshik <98695608+ViktorITshik@users.noreply.github.com>
2023-03-11 22:21:41 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,52 @@
-const swiper = new Swiper('.main-slider', {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-      nextEl: '.main-slider__arrow',
-    },
-});
+const bthDarkMode = document.querySelector(".dark-node-bth");
+
+
+
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    bthDarkMode.classList.add('dark-node-bth--active');
+    document.body.classList.add('dark');
+}
+
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+    bthDarkMode.classList.add('dark-node-bth--active');
+    document.body.classList.add('dark');
+} else if (localStorage.getItem('darkMode') === 'Light'){
+    bthDarkMode.classList.remove('dark-node-bth--active');
+    document.body.classList.remove('dark');
+
+}
+
+
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+const newColorScheme = event.matches ? 'dark': 'light';
+
+
+    if (newColorScheme === 'dark'){
+        bthDarkMode.classList.add('dark-node-bth--active');
+        document.body.classList.add('dark');
+        localStorage.setItem('darkMode', 'dark');
+    } else {
+        bthDarkMode.classList.remove('dark-node-bth--active');
+        document.body.classList.remove('dark');
+        localStorage.setItem('darkMode', 'Light');
+
+    }
+})
+
+
+bthDarkMode.onclick = function () {
+    bthDarkMode.classList.toggle("dark-node-bth--active");
+    const isDark = document.body.classList.toggle('dark');
+
+    if (isDark){
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'Light');
+    }
+
+}
+
